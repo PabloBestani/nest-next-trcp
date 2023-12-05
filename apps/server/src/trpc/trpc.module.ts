287d5f0c3076e88204a700936f46e43de344e574
Pablo Bestani <pablobestanimouk@gmail.com>
2023-12-05 11:36:15 -0300

    feat(trpc/server): created server side trpc config

---
+++ apps/server/src/trpc/trpc.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TrpcService } from './trpc.service';
+import { TrpcRouter } from './trpc.router';
+
+@Module({
+    imports: [],
+    controllers: [],
+    providers: [TrpcService, TrpcRouter],
+})
+
+export class TrpcModule {};
